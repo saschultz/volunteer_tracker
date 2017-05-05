eb40ba5b5d03cb1e27704edad27b6fe2cafb32a9
Sara Schultz <saschultz95@gmail.com>
2017-05-05 14:04:17 -0700

    add functionlity in app.rb to add project and display to uer

--- app.rb
+++ app.rb
@@ -3,3 +3,18 @@ require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
 require './lib/project'
 require './lib/volunteer'
+require 'pg'
+
+DB = PG.connect({:dbname => 'volunteer_tracker'})
+
+get '/' do
+  @projects = Project.all
+  erb :index
+end
+
+post '/add_project' do
+  name = params.fetch('name')
+  Project.new({:name => name}).save
+  @projects = Project.all
+  erb :index
+end
