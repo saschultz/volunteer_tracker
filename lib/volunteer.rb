dbab85acacf814ded3c7272617110bcf07ba8dbe
Sara Schultz <saschultz95@gmail.com>
2017-05-05 11:50:22 -0700

    pass spec to return name of volunteer in volunteer_spec.rb

---
+++ lib/volunteer.rb
@@ -0,0 +1,7 @@
+class Volunteer
+  attr_accessor :name
+
+  def initialize(attributes)
+    @name = attributes[:name]
+  end
+end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,1 +1,10 @@
 require 'spec_helper'
+
+describe Volunteer do
+  describe '#name' do
+    it 'tells you the name of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Wes Anderson'})
+      expect(test_volunteer.name).to(eq('Wes Anderson'))
+    end
+  end
+end
