8867b24466bed376cdae721cc01ac056301cebe3
Sara Schultz <saschultz95@gmail.com>
2017-05-05 14:39:01 -0700

    add functionality in UI to display list of volunteers for project

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ end
 
 post '/add_project' do
   name = params.fetch('name')
+  @project = Project.find(params.fetch("id").to_i)
+
   Project.new({:name => name}).save
   @projects = Project.all
   erb :index
@@ -21,18 +23,19 @@ end
 
 get '/project/:id' do
   @project = Project.find(params.fetch("id").to_i)
-
+  @volunteers =  DB.exec("SELECT name FROM volunteers WHERE project_id = #{params.fetch("id").to_i};")
   erb :project_interface
 end
 
 patch '/project/:id/edit' do
   @project = Project.find(params.fetch("id").to_i)
   name = params.fetch('name')
-
   if (name.split('').any?)
     @project.update({:name => name})
   else
     @project.update({:name => "#{@project.name}"})
   end
+  @volunteers =  DB.exec("SELECT name FROM volunteers WHERE project_id = #{params.fetch("id").to_i};")
+
   erb :project_interface
 end
